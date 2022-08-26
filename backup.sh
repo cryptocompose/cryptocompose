3e375b95fca710234a9ed101e4be1af9361b1527
yrzam <projects@yrz.am>
2022-08-26 21:30:09 +0100

    add persistent file to backup

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ echo Stopping containers...
 . stop.sh 2>/dev/null
 cd $base
 
+cp code/dynamic_config/persistent.conf data/persistent.conf
 echo -e Creating backup...
 sudo tar -czf backup.tar.gz \
   --exclude=data/bitcoind \
@@ -13,3 +14,4 @@ sudo tar -czf backup.tar.gz \
   --exclude=data/mongodb/diagnostic.data \
   --exclude=data/.gitkeep \
   data
+rm data/persistent.conf
