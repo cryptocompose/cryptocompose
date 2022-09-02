a5b03ef98b5da3b931eac1424e0eb0a6b44000ec
yrzam <projects@yrz.am>
2022-09-03 01:37:42 +0300

    add option to clear src on backup restore

--- restore-backup.sh
+++ restore-backup.sh
@@ -10,8 +10,8 @@ if [ ! -f backup.tar.gz ]; then
   echo No backup found.
   exit
 fi
-read -p "This will delete all existing data. Continue? (y/n) " decision
-if [ "$decision" != "y" ]; then
+read -p "This will delete all existing data. Continue? (y/n) " do_wipe
+if [ "$do_wipe" != "y" ]; then
   echo Cancelled.
   exit
 fi
@@ -22,5 +22,12 @@ rm -rf code/dynamic_config/*
 
 echo Unpacking archive...
 sudo tar -xf backup.tar.gz data code/dynamic_config
+
+read -p "Did versions change? (y/n) " do_clone_again
+if [ "$do_clone_again" == "y" ]; then
+  rm -rf code/src/*
+  . code/scripts/clone.sh
+  cd "$base"
+fi
 
 echo Done
