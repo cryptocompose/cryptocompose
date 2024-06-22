09fe2e855c92f0161e07c2fa508ad8458c03f255
Yuri Zamyatin <yuri@yrz.am>
2024-06-22 05:19:45 +0400

    bump versions

--- code/scripts/clone.sh
+++ code/scripts/clone.sh
@@ -3,16 +3,16 @@ echo -e "Clone started...\n"
 cd "$base/code/src"
 
 declare -A dirs=(
-  [bitcoind]="https://github.com/ruimarinho/docker-bitcoin-core ba147b689b9a78c60cf7d9fd5b20d8dfd2407e30"
-  [lnd]="https://github.com/lightningnetwork/lnd 909ba573ea49387171c143cb8c6759bc54fc82e4"
-  [lndinit]="https://github.com/lightninglabs/lndinit 67e363ea8ab562609017f6946b2832c380c811e2"
+  [bitcoind]="https://github.com/lightninglabs/docker-bitcoin-core 03b8411631cb29c842ad4afb8546dd327f96899e"
+  [lnd]="https://github.com/lightningnetwork/lnd 376b8cedc481a874d67d5bc38f63808737739d4c"
+  [lndinit]="https://github.com/lightninglabs/lndinit a5fa79d685bfa2570714723e61b72d6e00397370"
   [lndhub]="https://github.com/cryptocompose/lndhub f0e2c873895dd2790ae2abb1041be7d6f520032d"
   [lndhub-tg]="https://github.com/cryptocompose/lndhub-tg 158a8a0aa4c18a83f6da4c3a1bd2d513058d374a"
-  [thunderhub]="https://github.com/apotdevin/thunderhub 8d45e297e4b2140b3be3b4adca65aa3dcc0d93cc"
-  [nginx]="https://github.com/nginxinc/docker-nginx f3d86e99ba2db5d9918ede7b094fcad7b9128cd8"
+  [thunderhub]="https://github.com/apotdevin/thunderhub 0a9ae34eb3b5b076c245c5fa866d9c24caba7a4d"
+  [nginx]="https://github.com/nginxinc/docker-nginx a6f7d140744f8b15ff4314b8718b3f022efc7f43"
   [certbot]="https://github.com/certbot/certbot cb632c376f17dfd75306020a17248f3c33c1ab2f"
   [tor]="https://github.com/cryptocompose/docker-tor-meek 535c1fae86da444f876899dc67bba8b7986f58c1"
-  [redis]="https://github.com/docker-library/redis 5c0bbb5dfce3d4999649cbc3ba8bf7c123bcadff"
+  [redis]="https://github.com/docker-library/redis 845fcda9ba00c3dbc6ab8cd4b7e0efaa7be411f2"
   [mongodb]="https://github.com/docker-library/mongo 52c402d3744a806411b65e5fc843c65a87d8012c"
 )
 
